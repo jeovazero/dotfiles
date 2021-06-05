7da47824bd42a15300ae7e2652920936de407b8c
jeovazero <contato@jeova.ninja>
2021-06-04 21:08:59 -0300

    feat(nvim): add :set filetype for julia

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -1,9 +1,11 @@
 local ts = require 'nvim-treesitter.configs'
 
+vim.cmd "autocmd BufRead,BufNewFile *.jl :set filetype=julia"
+
 ts.setup {
     ensure_installed = {
-        'bash', 'c', 'cpp', 'css', 'dart', 'dockerfile', 'erlang', 'go',
-        'graphql', 'html', 'javascript', 'jsdoc', 'json', 'jsonc', 'julia',
+        'bash', 'c', 'cpp', 'css', 'dart', 'dockerfile', 'elm', 'erlang', 'go',
+        'graphql', 'haskell', 'html', 'javascript', 'jsdoc', 'json', 'jsonc', 'julia',
         'kotlin', 'lua', 'nix', 'python', 'regex', 'rust', 'scss', 'toml',
         'tsx', 'typescript', 'vue', 'yaml'
     },
