8f0f38a9cc213c2f3be3030a5db2534aa0cc9f9c
jeovazero <contato@jeova.ninja>
2021-09-01 01:03:26 -0300

    feat(nvim/treesitter): disable the haskell syntax

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -4,9 +4,11 @@ vim.cmd "autocmd BufRead,BufNewFile *.jl :set filetype=julia"
 vim.cmd "autocmd BufRead,BufNewFile *.nix :set filetype=nix"
 
 ts.setup {
+    -- 'haskell' has a bad performance for medium and big files
+    -- https://github.com/tree-sitter/tree-sitter-haskell/issues/41
     ensure_installed = {
         'bash', 'c', 'cpp', 'css', 'dart', 'dockerfile', 'elm', 'erlang', 'go',
-        'graphql', 'haskell', 'html', 'javascript', 'jsdoc', 'json', 'jsonc', 'julia',
+        'graphql', 'html', 'javascript', 'jsdoc', 'json', 'jsonc', 'julia',
         'kotlin', 'lua', 'nix', 'python', 'regex', 'rust', 'scss', 'toml',
         'tsx', 'typescript', 'vue', 'yaml'
     },
